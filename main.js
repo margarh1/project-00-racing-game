3b2518443e3828e4a86ff35fe986318e952bd27b
margarh1 <ha.margaret1@gmail.com>
2016-11-05 15:01:31 -0700

    wrote up wireframe, realized can create using js, attempting to make that work

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log('Sanity Check');
+
+$(document).on('ready', function() {
+  makeBoard();
+});
+
+function makeRow() {
+  for (var idx = 0; idx < 2; idx++) {
+    $('.container').append("<div class='row'>Row</div>");
+  }
+}
+
+function makeTrack() {
+  for (var idx = 0; idx < 2; idx++) {
+    $('.row').append("<div class='col-md-6 track'>This is a track</div>");
+  }
+};
+
+function makeGrid() {
+  for (var idx = 0; idx < 12; idx++) {
+    $('.track').append("<div class='col-md-1 grid'>Grid</div>");
+  }
+};
+
+function makeBoard() {
+  makeRow();
+  makeTrack();
+  makeGrid();
+}
